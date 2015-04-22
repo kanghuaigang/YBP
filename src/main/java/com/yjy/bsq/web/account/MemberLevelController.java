da1a8bc851cda177837171ec381328a39b663f08
亢怀刚 <huaigang.kang@163.com>
2015-04-22 23:41:09 +0800

    新增controller类

---
+++ src/main/java/com/yjy/bsq/web/account/MemberLevelController.java
@@ -0,0 +1,37 @@
+package com.yjy.bsq.web.account;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.yjy.bsq.entity.account.UserInfo;
+import com.yjy.bsq.entity.member.MemberLevel;
+import com.yjy.bsq.service.account.AccountService;
+
+@Controller
+@RequestMapping(value = "/memberLevel")
+public class MemberLevelController {
+	private static final int PAGE_SIZE = 10;
+	@Autowired
+	private AccountService accountService;
+	
+	@RequestMapping(value="add",method = RequestMethod.POST)
+	public String registerForm(Model model) {
+		model.addAttribute("memberLevel", new MemberLevel());
+		return "member/update";
+	}
+	
+	@ResponseBody
+	@RequestMapping(value="list",method = RequestMethod.POST)
+	public Page<UserInfo> list(
+			@RequestParam(value = "start") Integer start,
+			@RequestParam(value = "start", required = false) MemberLevel memberLevel) {
+		Page<UserInfo> userPages=accountService.getAllUser(start);
+		return userPages;
+	}
+}
